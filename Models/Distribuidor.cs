bc03a5ee050eb789c2b236af7c847857955a60ab
Bruno López Rodríguez <brunolopez25702blr@gmail.com>
2023-06-19 15:07:48 +0200

    Cambio de diseño en administración y sus validaciones

--- Models/Distribuidor.cs
+++ Models/Distribuidor.cs
@@ -7,14 +7,15 @@ namespace SullivanBurger.Models
   public class Distribuidor
   {
     [Key]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Nombre se encuentra vacío")]
     [StringLength(50, MinimumLength = 1, ErrorMessage = "La longitud del nombre es incorrecta")]
     public string Nombre { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Dirección se encuentra vacío")]
     [StringLength(255, MinimumLength = 5, ErrorMessage = "La dirección tiene 255 caracteres como máximo")]
     [DisplayName("Dirección")]
     public string Direccion { get; set; }
-    [Required]
-    [Range(600000000, 999999999, ErrorMessage = "El número de teléfono tiene que ser de 9 dígitos")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Teléfono se encuentra vacío")]
+    [Range(600000000, 999999999, ErrorMessage = "El número de Teléfono tiene que ser de 9 dígitos")]
     [DisplayName("Teléfono")]
     public int Telefono { get; set;}
 
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -7,26 +7,28 @@ namespace SullivanBurger.Models
   public class Producto
   {
     [Key]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Nombre se encuentra vacío")]
     [StringLength(100, MinimumLength = 1, ErrorMessage = "La longitud del nombre es incorrecta")]
     [DisplayName("Nombre")]
     public string Nombre { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Descripción se encuentra vacío")]
     [StringLength(400, MinimumLength = 5, ErrorMessage = "La longitud del email es incorrecta")]
     [DisplayName("Descripción")]
     public string Descripcion { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Precio se encuentra vacío")]
     [Range(0.5, 100, ErrorMessage = "El precio ha de encontrarse entre 0.50 y 100")]
     [DisplayName("Precio")]
     public float Precio { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Imagen se encuentra vacío")]
     [StringLength(100, MinimumLength = 5, ErrorMessage = "La longitud de la imagen es incorrecta")]
     [DisplayName("Imagen")]
     public string Imagen { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Tipo se encuentra vacío")]
     public string Tipo { get; set; }
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Stock se encuentra vacío")]
+    [Range(0, 200, ErrorMessage = "El stock ha de encontrarse entre 0 y 200")]
     public int Stock { get; set; } = 0;
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Distribuidor se encuentra vacío")]
     [ForeignKey("Distribuidor")]
     public string DistribuidorId { get; set; }
     public Distribuidor Distribuidor { get; set; }
